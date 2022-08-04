60ebe245d37509541dc0384600cf2b34e5a11c3b
ajp109 <57534418+ajp109@users.noreply.github.com>
2022-08-04 17:08:08 +0100

    Correct code in main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,27 @@
 #include "mbed.h"
 
+struct IntVector {
+  int x;
+  int y;
+};
+
+unsigned long long modSquared(IntVector vector) {
+    return vector.x*vector.x + vector.y*vector.y;
+}
+
 int main() {
-    while (true) {
-        sleep();
-    }
+  IntVector p;
+  IntVector q;
+  
+  p.x = -3;
+  p.y = 1;
+  
+  q = p;
+  q.x = 4;
+  
+  printf("(%d, %d).  Mod-squared %llu\r\n", q.x, q.y, modSquared(q));
+  printf("(%d, %d).  Mod-squared %llu\r\n", p.x, p.y, modSquared(p));
+  
+  // Do nothing, forever, when the program is complete
+  while (true) sleep();
 }
